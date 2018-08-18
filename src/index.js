6429bb9967a2ca4b4cfc4c8151ce407a43925ed5
Kaushalendra Kumar Singh <kksinghtomar.1996@gmail.com>
2018-08-18 14:48:46 +0530

    basic react app HelloWorld

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react"; //obj from module
+import ReactDOM from "react-dom";
+
+const element = <h1>welcome here!</h1>;
+ReactDOM.render(element, document.getElementById("root")); //elt to render
+//console.log(element);
+//React.createElement
